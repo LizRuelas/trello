3ef7711dfa445dec6637a1de94745ce94dab9d8f
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-22 15:05:02 -0800

    subiendo version 2

--- js/app.js
+++ js/app.js
@@ -11,16 +11,36 @@ window.addEventListener("load",function(){
 	span.addEventListener("click",function(e){
 		e.preventDefault();
 		mostrarFormulario();
-
 	});
 
-	
+	boton.addEventListener("click",function(e){
+		e.preventDefault();
+		mostrarLista();
+		texto.value = "";
+	});
 
 	function mostrarFormulario(){
 		box.style.display = "inline-block";
 		span.style.display = "none";
 		form.style.display = "inline-block";
 	}
-	
-	
+	function mostrarLista(){
+		box.style.display = "none";
+		span.style.display = "inline-block";
+		var lista = document.createElement("div");
+		var nodo = document.createTextNode(texto.value);
+		lista.classList.add("textBold");
+		lista.appendChild(nodo);
+		lista.classList.add("lista");
+		contenedor.insertBefore(lista,span.previousSibling);
+		lista.style.display = "inline-block";
+		lista.style.cssFloat = "left";
+
+		var nuevaTarjeta = document.createElement("a");
+		nuevaTarjeta.href= "#";
+		var nodoTarjeta =document.createTextNode("Añadir una tarjeta ...")
+		nuevaTarjeta.appendChild(nodoTarjeta);
+		lista.appendChild(nuevaTarjeta);
+		nuevaTarjeta.classList.add("tarjeta");
+	}
 });
