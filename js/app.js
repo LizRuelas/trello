465cb2284b10b134455d7776ccd27831a8097bb5
LizRuelas <liz.ruelas.borda@gmail.com>
2016-09-28 08:25:10 -0800

    subiendo cambios de insertbefore scroll en css

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ window.addEventListener("load",function(){
 		lista.classList.add("lista");
 		contenedor.insertBefore(lista,span.previousSibling);
 		lista.style.display = "inline-block";
-		lista.style.cssFloat = "left";
+		
 		var nuevaTarjeta = document.createElement("a");
 		nuevaTarjeta.href= "#";
 		var nodoTarjeta =document.createTextNode("Añadir una tarjeta ...")
@@ -42,13 +42,13 @@ window.addEventListener("load",function(){
 			var area = document.createElement("textarea");
 			area.classList.add("areaTexto");
 			lista.appendChild(area);
-			lista.insertBefore(area,nodo.nextSibling);
+			
 			var nuevoBoton = document.createElement("button");
 			var nodoBoton = document.createTextNode("Añadir");
 			nuevoBoton.classList.add("nuevoBoton");
 			nuevoBoton.appendChild(nodoBoton);
 			lista.appendChild(nuevoBoton);
-			lista.insertBefore(nuevoBoton,area.nextSibling);
+			
 		nuevoBoton.addEventListener("click",function(){
 			area.style.display= "none";
 			nuevoBoton.style.display="none";
@@ -58,7 +58,8 @@ window.addEventListener("load",function(){
 			listaTarjeta.classList.add("areaTarjeta");
 			listaTarjeta.appendChild(nodoListTarjeta);
 			lista.appendChild(listaTarjeta);
-			lista.insertBefore(listaTarjeta,nodo.nextSibling);
+			lista.insertBefore(nuevaTarjeta,nodoListTarjeta.previousSibling)
+			
 		}); 
 		});
 	}
