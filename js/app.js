a91a61eeb331a30742db901cef5ecd13a339f6fb
LizRuelas <liz.ruelas.borda@gmail.com>
2016-10-06 13:49:31 -0800

    agregrando version v2

--- js/app.js
+++ js/app.js
@@ -1,66 +1,96 @@
-window.addEventListener("load",function(){
+window.addEventListener("load" , cargarPagina);
+var contador = 0;
 	var span = document.getElementById("span");
 	var boton = document.getElementById("boton");
 	var texto = document.getElementById("texto");
 	var box = document.getElementById("box");
-	var form = document.getElementById("form");
 	var contenedor = document.getElementById("contenedor");
-	span.addEventListener("click",function(e){
+	
+function cargarPagina(){
+	span.addEventListener("click", añadirLista); 
+	boton.addEventListener("click", mostrarSpan);
+	boton.addEventListener("click", crearLista);
+}
+
+function añadirLista(e){
 		e.preventDefault();
-		mostrarFormulario();
-		texto.focus();
-	});
-	boton.addEventListener("click",function(e){
-		e.preventDefault();
-		mostrarLista();
-		texto.value = "";
-	});
-	function mostrarFormulario(){
 		box.style.display = "inline-block";
 		span.style.display = "none";
 		form.style.display = "inline-block";
-	}
-	function mostrarLista(){
+		texto.focus();
+}
+
+function mostrarSpan(e){
+	e.preventDefault();
 		box.style.display = "none";
 		span.style.display = "inline-block";
-		var lista = document.createElement("div");
-		var nodo = document.createTextNode(texto.value);
-		lista.classList.add("textBold");
-		lista.appendChild(nodo);
-		lista.classList.add("lista");
-		contenedor.insertBefore(lista,span.previousSibling);
-		lista.style.display = "inline-block";
-		
-		var nuevaTarjeta = document.createElement("a");
-		nuevaTarjeta.href= "#";
-		var nodoTarjeta =document.createTextNode("Añadir una tarjeta ...")
-		nuevaTarjeta.appendChild(nodoTarjeta);
-		lista.appendChild(nuevaTarjeta);
-		nuevaTarjeta.classList.add("tarjeta");
-		nuevaTarjeta.addEventListener("click", function(){
-			nuevaTarjeta.style.display = "none";
-			var area = document.createElement("textarea");
-			area.classList.add("areaTexto");
-			lista.appendChild(area);
-			
-			var nuevoBoton = document.createElement("button");
-			var nodoBoton = document.createTextNode("Añadir");
-			nuevoBoton.classList.add("nuevoBoton");
-			nuevoBoton.appendChild(nodoBoton);
-			lista.appendChild(nuevoBoton);
-			
-		nuevoBoton.addEventListener("click",function(){
-			area.style.display= "none";
-			nuevoBoton.style.display="none";
-			nuevaTarjeta.style.display="block"
-			var listaTarjeta = document.createElement("textarea");
-			var nodoListTarjeta = document.createTextNode(area.value);
-			listaTarjeta.classList.add("areaTarjeta");
-			listaTarjeta.appendChild(nodoListTarjeta);
-			lista.appendChild(listaTarjeta);
-			lista.insertBefore(nuevaTarjeta,nodoListTarjeta.previousSibling)
-			
-		}); 
-		});
-	}
-});
+}
+
+function crearLista(){
+	var lista = document.createElement("div");
+	lista.textContent = texto.value;
+	lista.classList.add("lista");
+	contenedor.insertBefore(lista,span.previousSibling);
+	texto.value = "";
+
+	var nuevaTarjeta = document.createElement("a");
+	nuevaTarjeta.href= "#";
+	var nodoTarjeta =document.createTextNode("Añadir una tarjeta ...")
+	nuevaTarjeta.appendChild(nodoTarjeta);
+	lista.appendChild(nuevaTarjeta);
+	nuevaTarjeta.classList.add("tarjeta");
+	nuevaTarjeta.addEventListener("click" , añadirNuevaTarjeta);
+
+	lista.addEventListener("drop" , soltar);
+	lista.addEventListener("dragover", dragover);
+
+}
+
+function añadirNuevaTarjeta(){
+	this.style.display = "none";
+	var form = document.createElement("form");
+	var area = document.createElement("textArea");
+	area.classList.add("areaTexto");
+	var nuevoBoton = document.createElement("button");
+	var nodoBoton = document.createTextNode("Añadir");
+	nuevoBoton.classList.add("nuevoBoton");
+	nuevoBoton.appendChild(nodoBoton);
+
+	form.appendChild(area);
+	form.appendChild(nuevoBoton);
+	this.parentElement.insertBefore(form,this.nextSibling)
+	nuevoBoton.addEventListener("click" , crearNuevaTarjeta)
+}
+
+function crearNuevaTarjeta(){
+	this.parentElement.style.display = "none";
+	this.parentElement.previousSibling.style.display = "block";
+	var textoTarjeta = document.createElement("textarea");
+	textoTarjeta.classList.add("areaTarjeta");
+	textoTarjeta.textContent = this.previousSibling.value;
+	this.parentElement.parentElement.insertBefore(textoTarjeta,this.parentElement.previousSibling);
+	textoTarjeta.draggable="true";
+	textoTarjeta.id = "id" + contador;
+	contador +=1;
+	textoTarjeta.addEventListener("dragstart" , empiezaArrastrar);
+}
+
+function empiezaArrastrar(e){
+	e.dataTransfer.setData("text", this.id);
+	console.log("empieza");
+}
+
+function soltar(e){
+	var elementoArrastrado = document.getElementById(e.dataTransfer.getData("text"));
+	this.insertBefore(elementoArrastrado , this.children[1]);
+}
+function dragover(e){
+	e.preventDefault();
+}
+
+
+
+
+
+
+
